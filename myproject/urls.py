0b9d954d006905767746a4ad10910cdbca08d85f
Rahul68964 <rahul844609@gmail.com>
2024-12-11 13:38:01 +0530

    Removed accounts module and associated project files for cleanup

--- myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-from accounts import views
-
-urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'), 
-    path('accounts/', include('accounts.urls')),
-    path('admin/', admin.site.urls),
-]
